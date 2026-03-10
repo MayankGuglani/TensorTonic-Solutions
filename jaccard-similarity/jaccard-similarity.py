77057b3092cf2d6fba2cae77c47e751f35977f1d
Mayank Guglani <168051884+MayankGuglani@users.noreply.github.com>
2026-03-10 15:58:57 +0530

    Update solution for jaccard-similarity

--- jaccard-similarity/jaccard-similarity.py
+++ jaccard-similarity/jaccard-similarity.py
@@ -1,21 +1,30 @@
 def jaccard_similarity(set_a, set_b):
-    set_un=set()
-    set_int=set()
+    # set_un=set()
+    # set_int=set()
     
-    for i in set_a:
-        if i in set_b:
-            set_int.add(i)
+    # for i in set_a:
+    #     if i in set_b:
+    #         set_int.add(i)
             
-    for i in set_a:
-        set_un.add(i)
+    # for i in set_a:
+    #     set_un.add(i)
         
-    for i in set_b:
-        set_un.add(i)
+    # for i in set_b:
+    #     set_un.add(i)
             
-    l=len(set_int)
-    p=len(set_un)
+    # l=len(set_int)
+    # p=len(set_un)
 
-    if p == 0:
-        return 0
+    # if p == 0:
+    #     return 0
     
+    # return l/p
+    a=set(set_a)
+    b=set(set_b)
+    int=a&b
+    un=a|b
+    l=len(int)
+    p=len(un)
+    if p==0:
+        return 0
     return l/p
